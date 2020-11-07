92f135e28adc21f1ab5a498196b2e2d4246ff8cb
Benjamin.Riou <Benjamin.Riou@adevinta.com>
2020-11-07 10:33:26 +0100

    Use of the QWERTY keycodes if required

--- keyboards/ergodox_ez/keymaps/ben75020/keymap.c
+++ keyboards/ergodox_ez/keymaps/ben75020/keymap.c
@@ -1,11 +1,13 @@
 #include QMK_KEYBOARD_H
 #include "version.h"
 #include "keymap_extras/keymap_colemak.h"
+#include "keymap_extras/sendstring_colemak.h"
 
 #define BASE 0 // default layer
+#define QWTY 1
 #define SYMB 2 // symbols
-#define MDIA 3 // media keys
-#define AIGU 4
+#define AIGU 3
+#define GRAV 4
 enum unicode_name {
     a_grave = 0x00e0, // à
     e_grave = 0x00e8, // è
@@ -16,7 +18,12 @@ enum unicode_name {
 
 enum custom_keycodes {
   VRSN = SAFE_RANGE,
-  AGRAV
+  AGRAV,
+  CEDIL,
+  EAIGU,
+  EGRAV,
+  OGRAV,
+  UGRAV
 };
 
 
@@ -76,11 +83,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,---------------------------------------------------.           ,--------------------------------------------------.
  * |Version  |  F1  |  F2  |  F3  |  F4  |  F5  |      |           |      |  F6  |  F7  |  F8  |  F9  |  F10 |   F11  |
  * |---------+------+------+------+------+------+------|           |------+------+------+------+------+------+--------|
- * |         |   !  |   @  |   {  |   }  |   |  |      |           |      |   Up |   7  |   8  |   9  |   *  |   F12  |
+ * |         | QWTY |   @  |   {  |   }  |   |  |      |           |      |   Up |   7  |   8  |   9  |   *  |   F12  |
  * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |         |   #  |   $  |   (  |   )  |   `  |------|           |------| Down |   4  |   5  |   6  |   +  |        |
  * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |         |   %  |   ^  |   [  |   ]  |   ~  |      |           |      |   &  |   1  |   2  |   3  |   \  |        |
+ * |         |   %  |   ^  | BASE |   ]  |   ~  |      |           |      |   &  |   1  |   2  |   3  |   \  |        |
  * `---------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  * | EEP_RST  |RESET|      |      |      |                                       |      |    . |   0  |   =  |      |
  *   `-----------------------------------'                                       `----------------------------------'
@@ -95,23 +102,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // SYMBOLS
 [SYMB] = LAYOUT_ergodox(
        // left hand
-       RGB_TOG,      KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_TRNS,
-       RGB_MOD,   KC_EXLM,  KC_AT,    KC_LCBR,  KC_RCBR,  KC_PIPE,  KC_TRNS,
-       KC_TRNS,   KC_HASH,  KC_DLR,   KC_LPRN,  KC_RPRN,  KC_GRV,
-       KC_TRNS,   KC_PERC,  KC_CIRC,  KC_LBRC,  KC_RBRC,  KC_TILD,  KC_TRNS,
-       EEP_RST,   RESET,KC_TRNS,KC_TRNS,KC_TRNS,
-                                       RGB_MOD,KC_TRNS,
-                                               KC_TRNS,
-                               RGB_VAD,RGB_VAI,KC_TRNS,
+       RGB_TOG,      KC_F1,    KC_F2,     KC_F3,      KC_F4,      KC_F5,    KC_TRNS,
+       RGB_MOD,      TG(QWTY), KC_AT,     KC_LCBR,    KC_RCBR,    KC_PIPE,  KC_TRNS,
+       KC_TRNS,      KC_HASH,  KC_DLR,    KC_LPRN,    KC_RPRN,    KC_GRV,
+       KC_TRNS,      KC_PERC,  KC_CIRC,   TG(BASE),   KC_RBRC,    KC_TILD,  KC_TRNS,
+       EEP_RST,      RESET,    KC_TRNS,   KC_TRNS,    KC_TRNS,
+                                          RGB_MOD,    KC_TRNS,
+                                                      KC_TRNS,
+                               RGB_VAD,   RGB_VAI,    KC_TRNS,
        // right hand
-       KC_TRNS, KC_F6,   KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_F11,
-       KC_TRNS, KC_UP,   KC_7,   KC_8,    KC_9,    KC_ASTR, KC_F12,
-                KC_DOWN, KC_4,   KC_5,    KC_6,    KC_PLUS, KC_TRNS,
-       KC_TRNS, KC_AMPR, KC_1,   KC_2,    KC_3,    KC_BSLS, KC_TRNS,
-                         KC_TRNS,KC_DOT,  KC_0,    KC_EQL,  KC_TRNS,
-       RGB_TOG, KC_TRNS,
+       KC_TRNS,      KC_F6,    KC_F7,     KC_F8,      KC_F9,      KC_F10,  KC_F11,
+       KC_TRNS,      KC_UP,    KC_7,      KC_8,       KC_9,       KC_ASTR, KC_F12,
+                     KC_DOWN,  KC_4,      KC_5,       KC_6,       KC_PLUS, KC_TRNS,
+       KC_TRNS,      KC_AMPR,  KC_1,      KC_2,       KC_3,       KC_BSLS, KC_TRNS,
+                               KC_TRNS,   KC_DOT,     KC_0,       KC_EQL,  KC_TRNS,
+       RGB_TOG,      KC_TRNS,
        KC_TRNS,
-       KC_TRNS, RGB_HUD, RGB_HUI
+       KC_TRNS,      RGB_HUD, RGB_HUI
 ),
 /* Keymap 2: Aecute accents
  *
@@ -122,15 +129,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |        |   à  |      |      |      |      |------|           |------|      |      |  é   |      |      |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
+ * |        |      |      |  ç   |      |      |      |           |      |      |      |      |      |      |        |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |      |      |      |      |      |                                       |      |      |      |      |      |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        |      |      |       |      |      |
  *                                 ,------|------|------|       |------+------+------.
- *                                 |      |      |      |       |      |      |Brwser|
- *                                 |      |      |------|       |------|      |Back  |
+ *                                 |      |      |      |       |      |      |      |
+ *                                 |      |      |------|       |------|      |      |
  *                                 |      |      |      |       |      |      |      |
  *                                 `--------------------'       `--------------------'
  */
@@ -138,8 +145,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [AIGU] = LAYOUT_ergodox(
        KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-       KC_TRNS, AGRAV,        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-       KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+       KC_TRNS, AGRAV,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+       KC_TRNS, KC_TRNS,     KC_TRNS, CEDIL,   KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS,
                                                KC_TRNS, KC_TRNS,
                                                         KC_TRNS,
@@ -147,15 +154,105 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // right hand
        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS,
-                 KC_TRNS, KC_TRNS, RALT(CM_E), KC_TRNS, KC_TRNS, KC_TRNS,
+                 KC_TRNS, KC_TRNS, EAIGU,      KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS,
                           KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS,
        KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS
 ),
+/* Keymap 2: Grave accents
+ *
+ * ,--------------------------------------------------.           ,--------------------------------------------------.
+ * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
+ * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
+ * |        |      |      |      |      |      |      |           |      |      |      |  ù   |      |      |        |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * |        |   à  |      |      |      |      |------|           |------|      |      |  è   |      |      |        |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * |        |      |      |  ç   |      |      |      |           |      |      |      |      |      |      |        |
+ * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
+ *   |      |      |      |      |      |                                       |      |      |      |      |      |
+ *   `----------------------------------'                                       `----------------------------------'
+ *                                        ,-------------.       ,-------------.
+ *                                        |      |      |       |      |      |
+ *                                 ,------|------|------|       |------+------+------.
+ *                                 |      |      |      |       |      |      |      |
+ *                                 |      |      |------|       |------|      |      |
+ *                                 |      |      |      |       |      |      |      |
+ *                                 `--------------------'       `--------------------'
+ */
+// accents graves
+[GRAV] = LAYOUT_ergodox(
+       KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+       KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+       KC_TRNS, AGRAV,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+       KC_TRNS, KC_TRNS,     KC_TRNS, CEDIL,   KC_TRNS, KC_TRNS, KC_TRNS,
+       KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS,
+                                               KC_TRNS, KC_TRNS,
+                                                        KC_TRNS,
+                                      KC_TRNS, KC_TRNS, KC_TRNS,
+    // right hand
+       KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS,
+       KC_TRNS,  KC_TRNS, KC_TRNS, UGRAV,      KC_TRNS, KC_TRNS, KC_TRNS,
+                 KC_TRNS, KC_TRNS, EAIGU,      KC_TRNS, KC_TRNS, KC_TRNS,
+       KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS,
+                          KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS,
+       KC_TRNS, KC_TRNS,
+       KC_TRNS,
+       KC_TRNS, KC_TRNS, KC_TRNS
+    ),
+/* Keymap 5: Qwerty Basic layer
+ *
+ * ,--------------------------------------------------.           ,--------------------------------------------------.
+ * |   =    |   1  |   2  |   3  |   4  |   5  | LEFT |           | RIGHT|   6  |   7  |   8  |   9  |   0  |   -    |
+ * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
+ * | Del    |   Q  |   W  |   E  |   R  |   T  |  L1  |           |  L1  |   Y  |   U  |   I  |   O  |   P  |   \    |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * | BkSp   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / L2|' / Cmd |
+ * |--------+------+------+------+------+------| Hyper|           | Meh  |------+------+------+------+------+--------|
+ * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |//Ctrl| RShift |
+ * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
+ *   |Grv/L1|  '"  |AltShf| Left | Right|                                       |  Up  | Down |   [  |   ]  | ~L1  |
+ *   `----------------------------------'                                       `----------------------------------'
+ *                                        ,-------------.       ,-------------.
+ *                                        | App  | LGui |       | Alt  |Ctrl/Esc|
+ *                                 ,------|------|------|       |------+--------+------.
+ *                                 |      |      | Home |       | PgUp |        |      |
+ *                                 | Space|Backsp|------|       |------|  Tab   |Enter |
+ *                                 |      |ace   | End  |       | PgDn |        |      |
+ *                                 `--------------------'       `----------------------'
+ */
+[QWTY] = LAYOUT_ergodox(
+        // left hand
+        KC_ESC,         KC_1,         KC_2,       KC_3,       KC_4,       KC_5,     KC_GRV,
+        KC_BSPC,        KC_Q,         KC_W,       KC_F,       KC_P,       KC_G,     MO(SYMB),
+        KC_TAB,         KC_A,         KC_R,       KC_S,       KC_T,       KC_D,
+        KC_LSHIFT,      KC_Z,         KC_X,       KC_C,       KC_V,       KC_B,     KC_MINS,
+
+        KC_RCTL,        MO(SYMB),     KC_LALT,    MO(AIGU),   RALT(CM_R),
+
+                                                                          KC_PSCR,  KC_LBRACKET,
+                                                                                    KC_HOME,
+                                                              KC_SPACE,   KC_BSPC,  KC_END,
+
+        // right hand
+        KC_EQUAL,       KC_6,         KC_7,       KC_8,       KC_9,       KC_0,     KC_BSPC,
+        MO(SYMB),       KC_J,         KC_L,       KC_U,       KC_Y,       KC_SCLN,  KC_BSLS,
+                        KC_H,         KC_N,       KC_E,       KC_I,       KC_O,     KC_QUOT,
+        KC_QUOT,        KC_K,         KC_M,       KC_COMMA,   KC_DOT,     KC_SLSH,  KC_RSHIFT,
+
+                                      KC_LEFT,    KC_DOWN,    KC_UP,      KC_RIGHT, KC_PAUSE,
+
+
+        KC_RBRACKET,    KC_DEL,
+        KC_PGUP,
+        KC_PGDN,        KC_LSHIFT,KC_ENT
+    ),
 };
 
+// MACROS DEFINITION
+//
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
     // dynamically generate these.
@@ -168,10 +265,32 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   // Test macro 1
     case AGRAV:
       if (record->event.pressed) {
-        // when keycode QMKBEST is pressed
-        SEND_STRING(SS_RALT("s") "a"); //here we suspect a bug, hence sending s to effectively having r instead
-      } else {
-        // when keycode QMKBEST is released
+        SEND_STRING(SS_RALT("r") "a");
+      }
+      break;
+    case CEDIL:
+      if (record->event.pressed) {
+        SEND_STRING(SS_RALT("c"));
+      }
+      break;
+    case EAIGU:
+      if (record->event.pressed) {
+        SEND_STRING(SS_RALT("e"));
+      }
+      break;
+    case EGRAV:
+      if (record->event.pressed) {
+        SEND_STRING(SS_RALT("r") "e");
+      }
+      break;
+    case OGRAV:
+      if (record->event.pressed) {
+        SEND_STRING(SS_RALT("r") "o");
+      }
+      break;
+    case UGRAV:
+      if (record->event.pressed) {
+        SEND_STRING(SS_RALT("r") "u");
       }
       break;
   }
@@ -179,6 +298,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 }
 
 // Runs just one time when the keyboard initializes.
+// This is for Linux Unicode, but we tend to avoid using it.
 void matrix_init_user(void) {
     set_unicode_input_mode(UC_LNX);
 };
